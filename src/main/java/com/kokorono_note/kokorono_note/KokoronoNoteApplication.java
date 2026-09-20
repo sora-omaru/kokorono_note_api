32eba01e612470e1e9cfccac46a56f350260b824
sora-omaru <konggongxia317@gmail.com>
2026-09-20 17:41:04 +0900

    最初のコミット

---
+++ src/main/java/com/kokorono_note/kokorono_note/KokoronoNoteApplication.java
@@ -0,0 +1,13 @@
+package com.kokorono_note.kokorono_note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KokoronoNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KokoronoNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kokorono_note/kokorono_note/KokoronoNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kokorono_note.kokorono_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KokoronoNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
